64d6fe42af8856da06e21369d4bccadbecab1c88
Daniel V <main@dvasquez4155.com>
2020-02-05 16:36:14 -0800

    $message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var values = getValues('Weather-Dashboard')
 var citiesContainer = $('#cities');
 $(values).each(function(index) {
     newCity(values[index])
@@ -15,12 +16,9 @@ function newCity(cityName) {
 $('#search').on('click', function() {
     var currentCity = $('#cityForm').val();
     getWeather(currentCity);
-    addValue(currentCity)
-    console.log(currentCity)
+    addValue('Weather-Dashboard',currentCity)
     if (values.indexOf(currentCity) == -1) {
         return;
     }
-    
-    console.log(currentCity)
     newCity(currentCity)
 })
